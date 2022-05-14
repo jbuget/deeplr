27446c610b560bce44411f689121e22cb3b8665a
jbuget <jbuget@gmail.com>
2022-05-14 10:00:50 +0200

    Perf: increase stability by improving performances

--- src/Translator.js
+++ src/Translator.js
@@ -5,63 +5,53 @@ export default class Translator {
   constructor() {
     const authKey = process.env.DEEPL_API_KEY;
     this._translator = new deepl.Translator(authKey);
+    this._count = 0;
   }
 
   async translateMultipleLangs(products, sourceLang, targetLangs) {
-
-    function sleep(ms) {
-      return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-      });
-    }
-
-    return targetLangs.reduce(async (results, targetLang) => {
-      console.log(`Translating: ${targetLang}`);
-
-      const translatedProducts = await this.translateSingleLang(products, sourceLang, targetLang);
-      results.push({
-        lang: targetLang,
-        products: translatedProducts
-      });
-
-      await sleep(1000);
-
-      return results;
-    }, []);
-
-
-    return await Promise.all(targetLangs.map(async (targetLang) => {
-      console.log(`Translating: ${targetLang}`);
+    return Promise.all(targetLangs.map(async (targetLang) => {
       const translatedProducts = await this.translateSingleLang(products, sourceLang, targetLang);
       return {
         lang: targetLang,
         products: translatedProducts
       };
-    }))
+    }));
   }
 
   async translateSingleLang(products, sourceLang, targetLang) {
-    const translatedProducts = await Promise.all(products.map(async (product) => {
+    const translatedProducts = [];
+    for (const product of products) {
       const translatedProduct = Object.assign({}, product);
 
+      const requests = {};
+
       if (product['Title'] && product['Title'].length > 0) {
-        translatedProduct['Title'] = (await this.translateText(product['Title'], sourceLang, targetLang, { preserveFormatting: true })).text;
-      } else {
-        translatedProduct['Title'] = 'ERROR! Field "Title" is empty or null'
+        requests['Title'] = this.translateText(product['Title'], sourceLang, targetLang, { preserveFormatting: true });
       }
       if (product['Body HTML'] && product['Body HTML'].length > 0) {
-        translatedProduct['Body HTML'] = (await this.translateText(product['Body HTML'], sourceLang, targetLang, { preserveFormatting: true, tagHandling: 'html' })).text;
-      } else {
-        translatedProduct['Body HTML'] = 'ERROR! Field "Body HTML" is empty or null';
+        requests['Body HTML'] = this.translateText(product['Body HTML'], sourceLang, targetLang, {
+          preserveFormatting: true,
+          tagHandling: 'html'
+        });
       }
-      return translatedProduct;
-    }));
+
+      const responses = await Promise.all(Object.values(requests));
+
+      for (const [key, value] of Object.entries(responses)) {
+        translatedProduct[key] = value;
+      }
+
+      translatedProducts.push(translatedProduct);
+    }
     return translatedProducts;
   }
 
   async translateText(text, sourceLang, targetLang, options) {
     try {
-      return this._translator.translateText(text, sourceLang, targetLang, options);
+      console.log(`count = ${this._count}`);
+      const result = await this._translator.translateText(text, sourceLang, targetLang, options);
+      this._count++;
+      return result.text;
     } catch (error) {
       console.error(error);
       return 'ERROR!'
